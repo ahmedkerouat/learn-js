092eaf736db8690dca25a5fd8fa28e9dd85dc54f
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-08 11:42:10 +0100

    moved into a folder

--- app.js
+++
@@ -1,9 +0,0 @@
-let clicksNumber = 0;
-let alreadyClicked = false;
-document.getElementById("clicks").innerText += 0;
-
-function clicked(){
-    clicksNumber += 1;
-    document.getElementById("clicks").innerText = "Clicks : " + clicksNumber;
-
-}
