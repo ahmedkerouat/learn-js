b1c77dbbdaa58d11d8f8f0d3b58561e1e3fdf5a5
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-15 19:07:10 +0100

    done

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,23 +1,35 @@
 const choicesList = ["rock", "paper", "scissors"];
+document.getElementById("other").style.display="none";
+function play(){
 
-function game(choice){
+}
+
+function game(choice, other1, other2){
     let random = Math.random();
     let randomIndex = Math.floor(random * choicesList.length);
     let randomChoice = choicesList[randomIndex];
 
+    clickedButton = document.getElementById(choice).style.display="block";
+    paperButton = document.getElementById(other1).style.display="none";
+    scissorsButton = document.getElementById(other2).style.display="none";
+    document.getElementById("other").style.display="block";
+    document.getElementById("other").style.backgroundImage ="url(imgs/"+randomChoice+".png)";
 if(choice == randomChoice){
-    //Nothing is actuallt supposed to happen lol
-    console.log("0|0");
+    //draw
+    document.getElementById("other").innerText =  "Draw.";
 }
 
 else if((choice == "rock" && randomChoice == "paper") || (choice =="scissors" && randomChoice == "rock" ) || (choice == "paper" && randomChoice == "scissors")){
     //lost
-    console.log(choice, randomChoice,"u lost");
+    document.getElementById("other").innerText =  "You lost.";
 }
 
 else{
     //win   
-    console.log(choice, randomChoice,"u won");
+    document.getElementById("other").innerText =  "You won.";
 }
 
+}
+function reset(){
+    location.reload();
 }
